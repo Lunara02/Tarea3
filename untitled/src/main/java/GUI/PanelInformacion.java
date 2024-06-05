e62238c5a1307b2b474c553f8a32d2a2fc4f2e5f
brai44 <braian.urba@gmail.com>
2024-06-05 18:17:30 -0400

    Creacion constructor PanelInformacion

--- untitled/src/main/java/GUI/PanelInformacion.java
+++ untitled/src/main/java/GUI/PanelInformacion.java
@@ -7,4 +7,10 @@ public class PanelInformacion extends JPanel {
     private Registrador Credito = new Registrador("Credito: 0");
     private Registrador IngreseProducto = new Registrador("Seleccione producto: ");
     private Registrador Error = new Registrador("");
+    public PanelInformacion(){
+        setLayout(new GridLayout(3,1));
+        add(Credito);
+        add(IngreseProducto);
+        add(Error);
+    }
 }
