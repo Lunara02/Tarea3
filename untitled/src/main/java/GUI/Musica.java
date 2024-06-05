a80d37350d6d282f1807b52ff56d7577c85174d2
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-06-05 09:40:08 -0400

    Feat Musica

--- untitled/src/main/java/GUI/Musica.java
+++ untitled/src/main/java/GUI/Musica.java
@@ -4,5 +4,16 @@ import javax.sound.sampled.*;
 import java.io.IOException;
 
 public class Musica {
+    private Clip clip;
 
+    public void playMusica(String s) {
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream(s + ".wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
