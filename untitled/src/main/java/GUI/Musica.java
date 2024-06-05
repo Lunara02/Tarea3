96aabc3a9ad75e25119d5b8840f27410406f7867
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-06-05 09:44:02 -0400

    Feat LoopMusica

--- untitled/src/main/java/GUI/Musica.java
+++ untitled/src/main/java/GUI/Musica.java
@@ -6,13 +6,16 @@ import java.io.IOException;
 public class Musica {
     private Clip clip;
 
-    public void playMusica(String s) {
+    public void playMusica(String s, int loop) {
         try {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream(s + ".wav"));
             clip = AudioSystem.getClip();
             clip.open(audioStream);
             FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             gainControl.setValue(-20.0f);
+            if (loop == 1) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
             clip.start();
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
             ex.printStackTrace();
