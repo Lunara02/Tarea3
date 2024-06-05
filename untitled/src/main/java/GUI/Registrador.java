89a38836230007e94a5b6739bf32eef5e79c4d58
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 14:36:25 -0400

    Creacion constructor Registrador

--- untitled/src/main/java/GUI/Registrador.java
+++ untitled/src/main/java/GUI/Registrador.java
@@ -4,5 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Registrador extends JLabel {
-
+    public Registrador(String Texto){
+        this.setBackground(Color.black);
+        this.setText(Texto);
+        this.setForeground(Color.GREEN);
+        this.setHorizontalAlignment(CENTER);
+        this.setOpaque(true);
+    }
 }
