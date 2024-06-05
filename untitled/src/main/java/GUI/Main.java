3ae8efada3c20c210def7c9dcdc50b96807a9e8b
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-06-05 09:37:22 -0400

    Feat Frame Principal

--- untitled/src/main/java/GUI/Main.java
+++ untitled/src/main/java/GUI/Main.java
@@ -1,10 +1,15 @@
 package GUI;
-
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Panel Principal");
+        PanelPrincipal panelPrincipal = new PanelPrincipal();
+        frame.add(panelPrincipal);
+        frame.setResizable(false);
+        frame.setSize(800, 630);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
 
