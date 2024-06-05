fbc4bc6bedc83043c5bb21eea013f0cdeff654d2
brai44 <braian.urba@gmail.com>
2024-06-05 18:16:44 -0400

    Creacion atributos PanelInformacion

--- untitled/src/main/java/GUI/PanelInformacion.java
+++ untitled/src/main/java/GUI/PanelInformacion.java
@@ -4,5 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelInformacion extends JPanel {
-
+    private Registrador Credito = new Registrador("Credito: 0");
+    private Registrador IngreseProducto = new Registrador("Seleccione producto: ");
+    private Registrador Error = new Registrador("");
 }
