14986f0c46b0981c8d17a9c95a2db70b5c8d19ad
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 20:22:56 -0400

    Creacion variables Depositos

--- untitled/src/main/java/GUI/Depositos.java
+++ untitled/src/main/java/GUI/Depositos.java
@@ -6,5 +6,9 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Depositos extends JPanel {
-
+    private Expendedora exp;
+    private Image Imagen;
+    private int tipo;
+    private int mettipo;
+    private ArrayList<PintarProducto> ProdDep;
 }
