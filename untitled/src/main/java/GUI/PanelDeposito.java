c7fd7ced6aae418270ca4cd48b52a79999090e28
brai44 <braian.urba@gmail.com>
2024-06-05 20:56:40 -0400

    feat: Creacion constructor PanelDeposito

--- untitled/src/main/java/GUI/PanelDeposito.java
+++ untitled/src/main/java/GUI/PanelDeposito.java
@@ -11,4 +11,19 @@ public class PanelDeposito extends JPanel{
     private Depositos DepSnickers;
     private Depositos DepSuper8;
     private Expendedora Ref;
+    public PanelDeposito(Expendedora exp){
+        this.setOpaque(false);
+        this.setLayout(new GridLayout(3,2,0,6));
+        this.Ref = exp;
+        DepCocacola = new Depositos(exp, new ImageIcon(getClass().getClassLoader().getResource("ProCocaCola.png")), 1,48,29);
+        DepSprite = new Depositos(exp, new ImageIcon(getClass().getClassLoader().getResource("ProSprite.png")), 2,28,29);
+        DepFanta = new Depositos(exp, new ImageIcon(getClass().getClassLoader().getResource("ProFanta.png")), 3,48,36);
+        DepSnickers = new Depositos(exp, new ImageIcon(getClass().getClassLoader().getResource("ProSnickers.png")), 4,40,18);
+        DepSuper8 = new Depositos(exp, new ImageIcon(getClass().getClassLoader().getResource("ProSuper8.png")), 5,60,24);
+        this.add(DepCocacola);
+        this.add(DepSprite);
+        this.add(DepFanta);
+        this.add(DepSnickers);
+        this.add(DepSuper8);
+    }
 }
