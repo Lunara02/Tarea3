b4c0aa95164e38ebc512cd5716e784a55557c68d
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-06-05 20:02:39 -0400

    Feat Panel de Relleno

--- untitled/src/main/java/GUI/PanelRelleno.java
+++ untitled/src/main/java/GUI/PanelRelleno.java
@@ -4,5 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelRelleno extends JPanel {
+    private Image Dep = null;
+    public PanelRelleno(int x, int y, Boolean TF){
+        this.setOpaque(TF);
+        this.setPreferredSize(new Dimension(x,y));
+    }
 
 }
