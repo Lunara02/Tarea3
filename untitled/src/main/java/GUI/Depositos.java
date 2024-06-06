a37f3321d1f0e62d42e5b0da4d8884ff3b566b25
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 22:45:14 -0400

    Doc Depositos

--- untitled/src/main/java/GUI/Depositos.java
+++ untitled/src/main/java/GUI/Depositos.java
@@ -5,27 +5,53 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * La clase Depositos extiende JPanel y representa un conjunto de productos en una máquina expendedora.
+ * Esta clase tiene dos constructores que permiten inicializar los depósitos de diferentes maneras,
+ * dependiendo de lo que se requiera.
+ * @author Sebastian Ignacio Vega Varela
+ */
 public class Depositos extends JPanel {
     private Expendedora exp;
     private Image Imagen;
     private int tipo;
     private int mettipo;
     private ArrayList<PintarProducto> ProdDep;
-    public Depositos(Expendedora exp, ImageIcon Imagen, int tipo, int X, int Y){
+
+    /**
+     * Constructor para crear un Depositos con una referencia a una Expendedora,
+     * una imagen, un tipo de producto, y posiciones iniciales.
+     * @param exp La referencia a la Expendedora principal.
+     * @param Imagen El icono de la imagen del producto.
+     * @param tipo El tipo de producto.
+     * @param X La posición inicial X.
+     * @param Y La posición inicial Y.
+     */
+    public Depositos(Expendedora exp, ImageIcon Imagen, int tipo, int X, int Y) {
         this.setOpaque(false);
         this.exp = exp;
         this.Imagen = Imagen.getImage();
         this.tipo = tipo;
         ProdDep = new ArrayList<>();
-        for(int x = 0; x < exp.getSizeProducto(tipo); x++){
+        for(int x = 0; x < exp.getSizeProducto(tipo); x++) {
             PintarProducto Pintura = new PintarProducto(Imagen, X + x*5, Y - x*4);
             ProdDep.add(Pintura);
         }
         mettipo = 1;
     }
-    public Depositos(ImageIcon Imagen, int tamaño, int X, int Y, int sumx, int sumy){
+
+    /**
+     * Constructor para crear un Depositos con una imagen, tamaño del depósito,
+     * posiciones iniciales y valores para colocar imagenes desfazada como se requiera.
+     * @param Imagen El icono de la imagen del producto.
+     * @param tamaño El tamaño del depósito (número de productos).
+     * @param X La posición inicial X.
+     * @param Y La posición inicial Y.
+     * @param sumx El incremento de la posición X para cada producto.
+     * @param sumy El decremento de la posición Y para cada producto.
+     */
+    public Depositos(ImageIcon Imagen, int tamaño, int X, int Y, int sumx, int sumy) {
         this.setOpaque(false);
-        this.exp = exp;
         this.Imagen = Imagen.getImage();
         this.tipo = tamaño;
         ProdDep = new ArrayList<>();
@@ -36,17 +62,20 @@ public class Depositos extends JPanel {
         mettipo = 2;
     }
 
+    /**
+     * Pinta los productos en el depósito.
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         int Can;
-        if(mettipo == 1) {
+        if(mettipo == 1){
             if (exp.getSizeProducto(tipo) > 5) {
                 Can = 5;
             } else {
                 Can = exp.getSizeProducto(tipo);
             }
-            for (int x = Can; x > 0; x--) {
+            for(int x = Can; x > 0; x--){
                 ProdDep.get(x - 1).paintComponent(g);
             }
         }
