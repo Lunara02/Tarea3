190912b0d9b3a3b8298e610fba45a98fa3449a8e
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 22:53:50 -0400

    Doc Registrador

--- untitled/src/main/java/GUI/Registrador.java
+++ untitled/src/main/java/GUI/Registrador.java
@@ -3,7 +3,16 @@ package GUI;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * La clase Registrador muestra texto en un panel con un fondo negro y texto verde.
+ * @author Sebastian Ignacio Vega Varela
+ */
 public class Registrador extends JLabel {
+
+    /**
+     * Crea una nueva instancia de Registrador con el texto especificado.
+     * @param Texto El texto que se mostrará en el Registrador.
+     */
     public Registrador(String Texto){
         this.setBackground(Color.black);
         this.setText(Texto);
@@ -11,7 +20,7 @@ public class Registrador extends JLabel {
         this.setHorizontalAlignment(CENTER);
         this.setOpaque(true);
     }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
