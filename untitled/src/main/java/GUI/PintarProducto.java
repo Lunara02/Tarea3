18948a8223b5b8e9f862f7bd4f6e181a7da52bd7
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-06-05 22:08:50 -0400

    Docs PintarProducto

--- untitled/src/main/java/GUI/PintarProducto.java
+++ untitled/src/main/java/GUI/PintarProducto.java
@@ -3,14 +3,31 @@ package GUI;
 import javax.swing.*;
 import java.awt.*;
 
-public class PintarProducto{
+/**
+ * La clase PintarProducto se encarga de dibujar una imagen en una posición específica dentro de un componente.
+ */
+public class PintarProducto {
     private ImageIcon Imagen;
     private int x, y;
-    public PintarProducto(ImageIcon Ruta, int x, int y){
+
+    /**
+     * Crea una nueva instancia de la clase PintarProducto.
+     *
+     * @param Ruta la ruta del archivo de imagen.
+     * @param x la coordenada x.
+     * @param y la coordenada y.
+     */
+    public PintarProducto(ImageIcon Ruta, int x, int y) {
         Imagen = Ruta;
         this.x = x;
         this.y = y;
     }
+
+    /**
+     * Dibuja la imagen en las coordenadas especificadas.
+     *
+     * @param g el contexto gráfico en el que se dibuja la imagen.
+     */
     protected void paintComponent(Graphics g) {
         g.drawImage(Imagen.getImage(), x, y, null);
     }
