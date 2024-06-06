ddf4c87c0839626a9827b52b128685e4e836150e
brai44 <braian.urba@gmail.com>
2024-06-05 20:54:50 -0400

    Creacion atributos PanelDeposito

--- untitled/src/main/java/GUI/PanelDeposito.java
+++ untitled/src/main/java/GUI/PanelDeposito.java
@@ -5,5 +5,10 @@ import java.awt.*;
 import javax.swing.*;
 
 public class PanelDeposito extends JPanel{
-
+    private Depositos DepCocacola;
+    private Depositos DepSprite;
+    private Depositos DepFanta;
+    private Depositos DepSnickers;
+    private Depositos DepSuper8;
+    private Expendedora Ref;
 }
