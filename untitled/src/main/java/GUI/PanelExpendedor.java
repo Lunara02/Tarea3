7d92a76bfa76798581570a903e4a17ac45eaff54
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 21:03:05 -0400

    Creacion variables PanelExpendedor

--- untitled/src/main/java/GUI/PanelExpendedor.java
+++ untitled/src/main/java/GUI/PanelExpendedor.java
@@ -6,5 +6,25 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class PanelExpendedor extends JPanel {
-
+    private Expendedora expendedora;
+    private BotonCuadrado CocaCola;
+    private BotonCuadrado Sprite;
+    private BotonCuadrado Fanta;
+    private BotonCuadrado Snickers;
+    private BotonCuadrado Super8;
+    private JButton cuantoVuelto = new JButton("");
+    private Musica musica = new Musica();
+    private BotonCuadrado EasterEgg = new BotonCuadrado(null,18,17,1);
+    private BotonCuadrado cuantoGanancias = new BotonCuadrado(new ImageIcon(getClass().getClassLoader().getResource("Bocallave.png")), 0,0,0);
+    private Registrador registrador = new Registrador("");
+    private PanelInformacion panelInformacion;
+    private PanelDeposito Deposito;
+    private Musica comprar = new Musica();
+    private Musica rap = new Musica();
+    private JFrame pedro = null;
+    private BotonCuadrado Rellenar;
+    private FrameDeposito DepVuelto = null;
+    private FrameGanancias DepGanancia;
+    private int magic = 0;
+    private int activable = 0;
 }
