6559b0277c6136497eae09484552b1224b1790f5
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 22:53:03 -0400

    Doc PanelPrincipal

--- untitled/src/main/java/GUI/PanelPrincipal.java
+++ untitled/src/main/java/GUI/PanelPrincipal.java
@@ -4,12 +4,21 @@ import EXPENDEDORA.*;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * PanelPrincipal es un JPanel que sirve como panel principal para la interfaz de usuario de la máquina expendedora.
+ * Contiene el PanelExpendedor (panel de la máquina expendedora), PanelComprador (panel del comprador), PanelInformacion (panel de información), PanelProducto (panel que muestra producto) y la expendedora principal
+ * @author Sebastian Ignacio Vega Varela
+ */
 public class PanelPrincipal extends JPanel {
     private PanelExpendedor panelExpendedor;
     private PanelComprador panelComprador;
     private PanelInformacion panelInformacion;
     private Expendedora expPrincipal = new Expendedora(5);
     private PanelProducto Product = new PanelProducto();
+
+    /**
+     * Constructor de PanelPrincipal con las configuraciones predeterminadas para crear la interfaz.
+     */
     public PanelPrincipal () {
         panelInformacion = new PanelInformacion();
         panelExpendedor = new PanelExpendedor(expPrincipal, panelInformacion, Product);
