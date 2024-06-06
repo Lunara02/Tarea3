af37fb18a17be650edc42f8939699f96a25fc133
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 22:47:25 -0400

    Doc FrameDeposito

--- untitled/src/main/java/GUI/FrameDeposito.java
+++ untitled/src/main/java/GUI/FrameDeposito.java
@@ -4,36 +4,47 @@ import EXPENDEDORA.Expendedora;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * La clase FrameDeposito extiende JFrame y que muestra las monedas de vuelto de una máquina expendedora.
+ * Esta clase crea un marco con un fondo personalizado y varios paneles que contienen las representaciones gráficas de las monedas.
+ * @author Sebastian Ignacio Vega Varela
+ */
 public class FrameDeposito extends JFrame {
     private PanelRelleno Fondo;
     private Depositos Moneda100;
     private Depositos Moneda500;
     private Depositos Moneda1000;
     private int m100 = 0, m500 = 0, m1000 = 0;
-    public FrameDeposito(String a, Image Dep, Expendedora Ref){
+
+    /**
+     * Constructor para crear un FrameDeposito.
+     * @param a El título del marco.
+     * @param Dep La imagen de fondo del marco.
+     * @param Ref La referencia a la Expendedora que contiene el vuelto a mostrar.
+     */
+    public FrameDeposito(String a, Image Dep, Expendedora Ref) {
         int vuelto = Ref.getVuelto();
         setResizable(false);
         setSize(314, 180);
         setVisible(true);
         setTitle(a);
-        Fondo = new PanelRelleno(Dep,0,0);
+        Fondo = new PanelRelleno(Dep, 0, 0);
         add(Fondo);
         Fondo.setLayout(new BorderLayout());
 
-        PanelRelleno Este = new PanelRelleno(40,0,false);
+        PanelRelleno Este = new PanelRelleno(40, 0, false);
         Fondo.add(Este, BorderLayout.EAST);
 
-        PanelRelleno Oeste = new PanelRelleno(39,0,false);
+        PanelRelleno Oeste = new PanelRelleno(39, 0, false);
         Fondo.add(Oeste, BorderLayout.WEST);
 
-        PanelRelleno Sur = new PanelRelleno(0,39,false);
-        Fondo.add(Sur,BorderLayout.SOUTH);
+        PanelRelleno Sur = new PanelRelleno(0, 39, false);
+        Fondo.add(Sur, BorderLayout.SOUTH);
 
-        PanelRelleno Centro = new PanelRelleno(0,0,false);
-        Centro.setLayout(new GridLayout(1,3));
+        PanelRelleno Centro = new PanelRelleno(0, 0, false);
+        Centro.setLayout(new GridLayout(1, 3));
 
         while (vuelto > 0) {
-            System.out.println(vuelto);
             if (vuelto >= 1000) {
                 m1000 += 1;
                 vuelto -= 1000;
@@ -46,9 +57,9 @@ public class FrameDeposito extends JFrame {
             }
         }
 
-        Moneda1000 = new Depositos(new ImageIcon(getClass().getClassLoader().getResource("moneda1000.png")), m1000, 12,55,0,8);
-        Moneda500 = new Depositos(new ImageIcon(getClass().getClassLoader().getResource("moneda500.png")), m500, 12,55,0,8);
-        Moneda100 = new Depositos(new ImageIcon(getClass().getClassLoader().getResource("moneda100.png")), m100, 12,55,0,8);
+        Moneda1000 = new Depositos(new ImageIcon(getClass().getClassLoader().getResource("moneda1000.png")), m1000, 12, 55, 0, 8);
+        Moneda500 = new Depositos(new ImageIcon(getClass().getClassLoader().getResource("moneda500.png")), m500, 12, 55, 0, 8);
+        Moneda100 = new Depositos(new ImageIcon(getClass().getClassLoader().getResource("moneda100.png")), m100, 12, 55, 0, 8);
 
         Centro.add(Moneda1000);
         Centro.add(Moneda500);
