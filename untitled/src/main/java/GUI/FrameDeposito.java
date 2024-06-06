29cd0e8bafff14b2d1c64d911638726ce2ea0004
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-06-05 20:29:10 -0400

    Creacion variables FrameDeposito

--- untitled/src/main/java/GUI/FrameDeposito.java
+++ untitled/src/main/java/GUI/FrameDeposito.java
@@ -5,5 +5,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class FrameDeposito extends JFrame {
-
+    private PanelRelleno Fondo;
+    private Depositos Moneda100;
+    private Depositos Moneda500;
+    private Depositos Moneda1000;
+    private int m100 = 0, m500 = 0, m1000 = 0;
 }
