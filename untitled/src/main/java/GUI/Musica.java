15f75fadc71a2a297b2dc9b96bf1870d034cc795
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-06-05 22:08:21 -0400

    Docs Musica

--- untitled/src/main/java/GUI/Musica.java
+++ untitled/src/main/java/GUI/Musica.java
@@ -3,9 +3,19 @@ package GUI;
 import javax.sound.sampled.*;
 import java.io.IOException;
 
+/**
+ * La clase Musica proporciona métodos para reproducir y detener archivos de audio en formato WAV.
+ * Utiliza javax.sound.sampled para manejar el audio.
+ */
 public class Musica {
     private Clip clip;
 
+    /**
+     * Reproduce un archivo de audio especificado y puede configurarse para que se repita en bucle.
+     *
+     * @param s el nombre del archivo de audio (sin la extensión .wav).
+     * @param loop si es 1, el audio se reproducirá en un bucle continuo; de lo contrario, se reproducirá una vez.
+     */
     public void playMusica(String s, int loop) {
         try {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream(s + ".wav"));
@@ -21,6 +31,10 @@ public class Musica {
             ex.printStackTrace();
         }
     }
+
+    /**
+     * Detiene la reproducción del archivo de audio si está en ejecución y libera los recursos.
+     */
     public void stopMusica() {
         if (clip != null && clip.isRunning()) {
             clip.stop();
